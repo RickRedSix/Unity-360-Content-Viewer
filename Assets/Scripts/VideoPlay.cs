f6fe24cf5c48fb9a5d8e3a1b521bd5818211b0b1
Rick Smith <rick.smith@analogfolk.com>
2016-11-16 12:52:59 +0000

    added project without video

---
+++ Assets/Scripts/VideoPlay.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoPlay : MonoBehaviour {
+
+	MovieTexture movTex;
+
+	void Start()
+	{
+		movTex = (MovieTexture)GetComponent<Renderer>().material.mainTexture;
+		movTex.Play(); // Autoplay on start
+		movTex.loop = true; // Loop forever
+	}
+
+}
